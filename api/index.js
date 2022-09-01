939346b54c5b5a1dcfbff02ed6bbc4a60471677f
Zac Burrage <zac.burrage@okta.com>
2022-09-01 10:54:06 -0500

    change domain back

--- api/index.js
+++ api/index.js
@@ -63,12 +63,6 @@ const checkJwt = jwt({
   algorithms: ['RS256'],
 });
 
-app.get('/api', (req, res) => {
-  const path = `/api/item/${v4()}`;
-  res.setHeader('Content-Type', 'text/html');
-  res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate');
-  res.end(`Hello! Go to item: <a href="${path}">${path}</a>`);
-});
 
 app.get('/api/external', (req, res) => {
 
